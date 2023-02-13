5b3b2deeacf89e6bac4017c6b6af8afc6585ce46
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-13 17:18:31 +0200

    range

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Child from './Child';
+import React, { useState } from 'react';
 
 function App() {
-
+  const [s1, setS1] = useState(50);
+  let a = 50;
   function changeHandler(event) {
-    let a = event.target.value;
+    a = event.target.value;
     console.log(a)
+    setS1(a)
   }
 
   return (
     <>
       <div>
         <input type="range" onChange={changeHandler} />
-    </div>
+      </div>
+      <Child p1={s1} />
     </>
   );
 }
---
+++ src/Child.js
@@ -0,0 +1,14 @@
+
+function Child(props) {
+    return (
+        <>
+            <div>
+                <h3>Child</h3>        
+                <div>{props.p1}</div>
+            </div>
+            
+        </>
+    )
+}
+
+export default Child;
